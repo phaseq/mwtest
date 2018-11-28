d3f1ae34f4ac06b0ff035a1fbb2beab2fa556175
Fabian Beckmann <fb@phaseq.de>
2018-11-28 23:31:34 +0100

    file renaming

--- src/report.rs
+++ src/report.rs
@@ -14,11 +14,12 @@ impl<'a> Report<'a> {
         let report = Report {
             std_out: CliLogger { verbose: verbose },
             file_logger: FileLogger::new(&artifacts_root),
-            xml_report: XmlReport::new(&xml_location, &testcases_root).unwrap(),
+            xml_report: XmlReport::new(&xml_location, &artifacts_root, &testcases_root).unwrap(),
         };
         report.std_out.new();
         report
     }
+
     pub fn add(
         &mut self,
         i: usize,
@@ -41,19 +42,24 @@ impl<'a> Report<'a> {
 
 struct XmlReport<'a> {
     file: File,
-    file_path: PathBuf,
     results: HashMap<String, Vec<(::TestInstance<'a>, ::TestCommandResult)>>,
-    testcases_root: String,
+    artifacts_root: PathBuf,
+    testcases_root: PathBuf,
 }
 impl<'a> XmlReport<'a> {
-    fn new(path: &Path, testcases_root: &str) -> std::io::Result<XmlReport<'a>> {
+    fn new(
+        path: &Path,
+        artifacts_root: &Path,
+        testcases_root: &str,
+    ) -> std::io::Result<XmlReport<'a>> {
         Ok(XmlReport {
             file: File::create(&path)?,
-            file_path: PathBuf::from(path),
             results: HashMap::new(),
-            testcases_root: testcases_root.to_string(),
+            artifacts_root: PathBuf::from(artifacts_root),
+            testcases_root: PathBuf::from(testcases_root),
         })
     }
+
     fn add(&mut self, test_instance: ::TestInstance<'a>, test_result: &::TestCommandResult) {
         match self.results.entry(test_instance.app_name.to_string()) {
             hash_map::Entry::Vacant(e) => {
@@ -64,13 +70,14 @@ impl<'a> XmlReport<'a> {
             }
         }
     }
+
     fn write(&mut self) -> std::io::Result<()> {
         let mut out = BufWriter::new(&self.file);
         out.write(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?><mwtest>")?;
         out.write(
             format!(
                 "<config><reference_root>{}</reference_root></config>",
-                self.testcases_root
+                self.testcases_root.to_string_lossy().into_owned()
             ).as_bytes(),
         )?;
         out.write(b"<testsuites>")?;
@@ -83,52 +90,103 @@ impl<'a> XmlReport<'a> {
                 ).as_bytes(),
             )?;
             for (test_instance, command_result) in test_results.iter() {
-                out.write(
-                    format!(
-                        "<testcase name=\"{}\">",
-                        htmlescape::encode_attribute(&test_instance.test_id.id)
-                    ).as_bytes(),
-                )?;
-                out.write(
-                    format!("<exit_code>{}</exit_code>", command_result.exit_code).as_bytes(),
-                )?;
-                if command_result.exit_code != 0 {
-                    out.write(b"<failure />")?;
-                }
-                out.write(
-                    format!(
-                        "<system_out>{}</system_out>",
-                        htmlescape::encode_minimal(&command_result.stdout)
-                    ).as_bytes(),
-                )?;
-                if let Some(tmp_dir) = &test_instance.command.tmp_path {
-                    if PathBuf::from(tmp_dir).exists() {
-                        let rel_tmp_dir = PathBuf::from(tmp_dir);
-                        let rel_tmp_dir = rel_tmp_dir
-                            .strip_prefix(self.file_path.parent().unwrap())
-                            .unwrap();
-                        let rel_tmp_dir = rel_tmp_dir.to_string_lossy().into_owned();
-                        let rel_reference_path = &test_instance
-                            .test_id
-                            .rel_path
-                            .as_ref()
-                            .unwrap()
-                            .to_string_lossy()
-                            .into_owned();
-                        out.write(
-                            format!(
-                                "<artifact reference=\"{}\" location=\"{}\" />",
-                                htmlescape::encode_attribute(&rel_reference_path),
-                                htmlescape::encode_attribute(&rel_tmp_dir)
-                            ).as_bytes(),
-                        )?;
-                    }
-                }
-                out.write(b"</testcase>")?;
+                self.write_testcase(&mut out, &test_instance, &command_result)?;
             }
             out.write(b"</testsuite>")?;
         }
         out.write(b"</testsuites></mwtest>")?;
+        Ok(())
+    }
+
+    fn write_testcase(
+        &self,
+        out: &mut BufWriter<&File>,
+        test_instance: &::TestInstance<'a>,
+        command_result: &::TestCommandResult,
+    ) -> std::io::Result<()> {
+        out.write(
+            format!(
+                "<testcase name=\"{}\">",
+                htmlescape::encode_attribute(&test_instance.test_id.id)
+            ).as_bytes(),
+        )?;
+        out.write(format!("<exit_code>{}</exit_code>", command_result.exit_code).as_bytes())?;
+        if command_result.exit_code != 0 {
+            out.write(b"<failure />")?;
+        }
+        out.write(
+            format!(
+                "<system_out>{}</system_out>",
+                htmlescape::encode_minimal(&command_result.stdout)
+            ).as_bytes(),
+        )?;
+        if let Some(tmp_path) = &test_instance.command.tmp_path {
+            if tmp_path.exists() {
+                /*let rel_tmp_dir = tmp_path
+                    .strip_prefix(&self.artifacts_root)
+                    .unwrap()
+                    .to_string_lossy()
+                    .into_owned();
+                let rel_reference_path = &rel_path.to_string_lossy().into_owned();*/
+                //
+                let rel_path = test_instance.test_id.rel_path.as_ref().unwrap();
+                let abs_reference_path = self.testcases_root.join(rel_path);
+                let sub_dir = if command_result.exit_code == 0 {
+                    "success"
+                } else {
+                    "different"
+                };
+                let abs_artifact_path = self.artifacts_root.join(sub_dir).join(rel_path);
+                if abs_reference_path.is_dir() || tmp_path.is_file() {
+                    std::fs::create_dir_all(abs_artifact_path.parent().unwrap())?;
+                    std::fs::rename(&tmp_path, &abs_artifact_path)?;
+                    self.write_artifact(out, &abs_artifact_path, &abs_artifact_path)?;
+                } else {
+                    std::fs::create_dir_all(&abs_artifact_path)?;
+                    for entry in std::fs::read_dir(tmp_path)? {
+                        let from = entry?.path();
+                        let file_name = &from.file_name().unwrap();
+                        let to = abs_reference_path.join(file_name);
+                        std::fs::rename(&from, &to)?;
+                        self.write_artifact(out, &abs_artifact_path, &abs_artifact_path)?;
+                    }
+                }
+                //
+                /*out.write(
+                    format!(
+                        "<artifact reference=\"{}\" location=\"{}\" />",
+                        htmlescape::encode_attribute(&rel_reference_path),
+                        htmlescape::encode_attribute(&rel_tmp_dir)
+                    ).as_bytes(),
+                )?;*/            }
+        }
+        out.write(b"</testcase>")?;
+        Ok(())
+    }
+
+    fn write_artifact(
+        &self,
+        out: &mut BufWriter<&File>,
+        abs_reference_path: &Path,
+        abs_artifact_path: &Path,
+    ) -> std::io::Result<()> {
+        let rel_reference_path = abs_reference_path
+            .strip_prefix(&self.testcases_root)
+            .unwrap()
+            .to_str()
+            .unwrap();
+        let rel_artifact_path = abs_artifact_path
+            .strip_prefix(&self.artifacts_root)
+            .unwrap()
+            .to_str()
+            .unwrap();
+        out.write(
+            format!(
+                "<artifact reference=\"{}\" location=\"{}\" />",
+                htmlescape::encode_attribute(&rel_reference_path),
+                htmlescape::encode_attribute(&rel_artifact_path)
+            ).as_bytes(),
+        )?;
         Ok(())
     }
 }
