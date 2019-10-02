d144d87505734b5e78d1a8967b4fcb5de52dd93f
Fabian Beckmann <fb@phaseq.de>
2019-10-02 20:07:04 +0200

    fix test

--- src/bin/mock_xge.rs
+++ src/bin/mock_xge.rs
@@ -15,7 +15,32 @@ fn accept_commands(stream: TcpStream) {
         }
         let request: serde_json::Result<xge_lib::StreamRequest> = serde_json::from_str(&cmd_str);
         if let Ok(request) = request {
-            report(request.id, 0, "example output");
+            let mut args = request.command.iter();
+            let mut cmd = std::process::Command::new(args.next().unwrap());
+            for arg in args {
+                cmd.arg(arg);
+            }
+
+            let maybe_output = cmd.output();
+            match maybe_output {
+                Ok(output) => {
+                    let exit_code = output.status.code().unwrap_or(-7787);
+                    let stdout =
+                        str::from_utf8(&output.stdout).unwrap_or("couldn't decode output!");
+                    let stderr =
+                        str::from_utf8(&output.stderr).unwrap_or("couldn't decode output!");
+                    let output_str = stderr.to_owned() + stdout;
+
+                    report(request.id, exit_code, &output_str);
+                }
+                Err(e) => {
+                    report(
+                        request.id,
+                        -7787,
+                        &format!("XGE-Launcher: failed to execute process: {}", e),
+                    );
+                }
+            }
         }
     }
     println!("mwt done");
